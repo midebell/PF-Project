99c26412b6e3d89ecc2db40740c79ccfa8b1149b
midebell <35881982+midebell@users.noreply.github.com>
2018-08-10 23:26:40 -0400

    Add files via upload

---
+++ No 1/vars.tf
@@ -0,0 +1,17 @@
+variable "AWS_REGION" {
+  default = "us-east-1"
+}
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "pfkey"
+}
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "pfkey.pub"
+}
+variable "AMIS" {
+  type = "map"
+  default = {
+       us-east-1 = "ami-ef151d90"
+       us-west-2 = "ami-1cc69e64"
+       us-west-1 = "ami-d8b05cbb"
+  }
+}
