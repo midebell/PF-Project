6d9f6709e687dcae48e9ea9adfa502c3b6cd3757
midebell <oluweezy@yahoo.com>
2018-08-11 02:01:44 -0400

    edit folder No 1

--- No_1/vars.tf
+++ No_1/vars.tf
@@ -1,14 +1,8 @@
-variable "AWS_ACCESS_KEY" {}
-variable "AWS_SECRET_KEY" {}
+
 variable "AWS_REGION" {
   default = "us-east-1"
 }
-variable "PATH_TO_PRIVATE_KEY" {
-  default = "pfkey"
-}
-variable "PATH_TO_PUBLIC_KEY" {
-  default = "pfkey.pub"
-}
+
 variable "AMIS" {
   type = "map"
   default = {
